2de3d05598ffef774576b7c3e528dda3452bef58
eliachow <eliacchow@gmail.com>
2023-07-03 12:10:44 -0700

    find stores' revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@company = Store.all
+
+revenue_array = []
+@company.each do |store|
+  revenue_array << store.annual_revenue
+end
+
+puts "👉👉👉 Total Company Revenue: $#{revenue_array.sum}"
+puts "👉👉👉 Average Company Revenue: $#{revenue_array.sum / revenue_array.count}"
+
+@stores_over_1M = Store.where("annual_revenue >= ? ", 1000000).size
+
+puts "Numbers of stores with annual sales over 1M: #{@stores_over_1M}"
