162b78a80c39ddd281cbd1eaa3baa36a5d89cd83
eliachow <eliacchow@gmail.com>
2023-07-03 11:14:59 -0700

    find and update stores

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+puts "👉👉👉 @store1: #{@store1.name}"
+
+@store2 = Store.second
+puts "👉👉👉 @store2: #{@store2.name}"
+
+@store1.name = 'Mount Pleasant'
+@store1.save
+puts "👉👉👉 @store1: #{@store1.name}"
