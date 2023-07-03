c0f60a33d4bb080d96f6b67cb43d26ac6e4ba322
eliachow <eliacchow@gmail.com>
2023-07-03 12:48:20 -0700

    create validations for employee and store entries

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,36 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { 
+    only_integer: true, greater_than_or_equal_to: 0}
+  validate :must_carry_apparel
+
+  def must_carry_apparel
+    if !mens_apparel? && !womens_apparel?
+      errors.add(:base, "must carry at least one of men's or women's apparel")
+    end
+  end
+end
+
+puts "what is the store name?"
+store_name = gets.chomp
+store = Store.new(name: store_name)
+if store.save
+  puts "Store created successfully!"
+else
+  puts "Errors occurred while creating the store:"
+  store.errors.full_messages.each do |message|
+    puts message
+  end
+end
+
+
